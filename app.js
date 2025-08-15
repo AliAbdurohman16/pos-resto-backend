ae50b00b8e5f8fc439ce1880933345a89d415bdb
AliAbdurohman16 <alyabdurohman16@gmail.com>
2025-08-15 15:29:55 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const config = require('./config/config');
+const globalErrorHandler = require('./middlewares/globalErrorHandler');
+const createHttpError = require('http-errors');
+const app = express();
+
+const PORT = config.port;
+connectDB();
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+
+// Root Endpoint
+app.get('/', (req, res) => {
+  res.json({message: 'Hello from POS Server!'});
+});
+
+// Other Endpoints
+app.use('/api/users', require('./routes/userRoute'));
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+// Server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
